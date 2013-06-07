b83e90ed2b84c5acfc7c2883345670f15538c877
timp21337 <timp@paneris.org>
2013-06-07 21:32:09 +0100

    Remove rubbish

--- ucl/management/commands/load.py
+++ ucl/management/commands/load.py
@@ -1,11 +1,4 @@
-from datetime import date
 from django.core.management.base import BaseCommand, CommandError
-from members.models import create_adult
-from members.models import create_child
-from members.models import create_doctor
-from members.models import create_backup
-from members.models import Session
-from members.models import Attendance
 
 
 from django.db import connection
